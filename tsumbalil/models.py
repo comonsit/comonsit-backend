1ff2b5c198b9189dc91f833ca612cdbbfd26ec95
mauricioinaz <mauricio.glez@gmail.com>
2019-11-18 18:34:32 -0500

    minor model representations django

--- tsumbalil/models.py
+++ tsumbalil/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Cargos(models.Model):
     nombre_de_cargo = models.CharField(max_length=40, blank=False)
 
+    def __str__(self):
+        return "{nombre}".format(nombre=self.nombre_de_cargo)
+
 
 class Regiones(models.Model):
     nombre_de_region = models.CharField(max_length=40, blank=False)
@@ -15,3 +18,6 @@ class Regiones(models.Model):
 class Comunidades(models.Model):
     nombre_de_comunidad = models.CharField(max_length=40, blank=False)
     region = models.ForeignKey(Regiones, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return "{nombre}".format(nombre=self.nombre_de_comunidad)
