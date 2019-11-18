41c5b9df520b540b1a47c27e01e4f9666b8a42ff
mauricioinaz <mauricio.glez@gmail.com>
2019-11-18 17:26:40 -0500

    adjust automatic date acopios

--- acopios/models.py
+++ acopios/models.py
@@ -15,7 +15,7 @@ class Acopios(models.Model):
 
     id = models.AutoField(primary_key=True)  # TODO: Público
     # claveSocio = models.ForeignKey(Users.claveSocio, on_delete=models.CASCADE, blank=False)  # TODO: on delete?
-    fecha = models.DateField(auto_now_add=True)  # TODO: NO usar como timespamp?
+    fecha = models.DateField()  # TODO: NO usar como timespamp?
     ingreso = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
     kilos_de_producto = models.IntegerField(blank=True, null=True)  # TODO: meter límites máximo y mínimo?
     tipo_de_producto = models.CharField(max_length=2, choices=PRODUCTO_CHOICES, blank=True)
