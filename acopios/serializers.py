d421e84f1acf4411c1e72b77c731babc81dd8f5f
mauricioinaz <mauricio.glez@gmail.com>
2019-11-19 09:36:06 -0500

    fix serializer

--- acopios/serializers.py
+++ acopios/serializers.py
@@ -5,4 +5,4 @@ from .models import Acopios
 class AcopiosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Acopios
-        fields = ['id', 'fecha', 'ingreso', 'kilosDeProducto', 'tipoDeProducto']
+        fields = ['id', 'fecha', 'ingreso', 'kilos_de_producto', 'tipo_de_producto']
