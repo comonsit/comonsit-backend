bdb07beb90c512f8e05bafa8e056f3eaf8792f3e
mauricioinaz <mauricio.glez@gmail.com>
2019-11-25 14:58:23 -0500

    RE: edit socio information in backend

--- socios/serializers.py
+++ socios/serializers.py
@@ -5,18 +5,17 @@ from tsumbalil.serializers import ComunidadesSerializer
 
 
 class SociosSerializer(serializers.ModelSerializer):
-    comunidad = ComunidadesSerializer()
-    # comunidad = serializers.HyperlinkedRelatedField(queryset=Comunidades.objects.all(), view_name='comunidad-detail')
-    cargo = serializers.SerializerMethodField()
+    comunidad_id = ComunidadesSerializer(source='comunidad', read_only=True)
+    cargo_id = serializers.SerializerMethodField(source='cargo', read_only=True)
 
     class Meta:
         model = Socios
-        fields = ['clave_socio', 'nombres', 'apellidos', 'comunidad',
+        fields = ['clave_socio', 'nombres', 'apellidos', 'comunidad_id',
                   'curp', 'telefono', 'fecha_nacimiento', 'fecha_ingr_yomol_atel', 'fecha_ingr_programa',
-                  'cargo', 'prod_trab', 'clave_anterior', 'estatus_cafe', 'estatus_miel',
+                  'cargo_id', 'prod_trab', 'clave_anterior', 'estatus_cafe', 'estatus_miel',
                   'estatus_yip', 'estatus_gral']
 
-    def get_cargo(self, socio):
+    def get_cargo_id(self, socio):
         if socio.cargo:
             return '{0}'.format(socio.cargo.nombre_de_cargo)
         return ''
