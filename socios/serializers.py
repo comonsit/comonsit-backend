1175462b207771a35d8873b898ccb66f5e793a86
mauricioinaz <mauricio.glez@gmail.com>
2020-01-10 14:09:56 -0500

    RE: Update Socios List order

--- socios/serializers.py
+++ socios/serializers.py
@@ -4,11 +4,16 @@ from .models import Socio
 
 class SocioSerializer(serializers.ModelSerializer):
     region = serializers.SerializerMethodField(read_only=True)
+    nombre_comunidad = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Socio
         fields = "__all__"
 
     def get_region(self, object):
-        region = object.comunidad.region.nombre_de_region
+        region = object.comunidad.region.id
         return region
+
+    def get_nombre_comunidad(self, object):
+        nombre_comunidad = object.comunidad.nombre_de_comunidad
+        return nombre_comunidad
