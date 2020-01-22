d7093c0b804a13833ce05c8f483adbd199c816fd
mauricioinaz <mauricio.glez@gmail.com>
2020-01-22 17:16:18 -0500

    DJ: Check for recent duplicates in SolicitudCredito creation

--- creditos/serializers.py
+++ creditos/serializers.py
@@ -15,4 +15,13 @@ class SolicitudCreditoSerializer(serializers.ModelSerializer):
         """
         if data['clave_socio'] == data['aval']:
             raise serializers.ValidationError("Aval y Solicitante deben ser diferentes")
+        """
+        Check for recent duplicates
+        """
+        if SolicitudCredito.objects.filter(
+           clave_socio=data['clave_socio']).filter(
+           fecha_solicitud=data['fecha_solicitud']).filter(
+           monto_solicitado=data['monto_solicitado']):
+            raise serializers.ValidationError("Ya existe un crédito con esa fecha y monto")
+
         return data
