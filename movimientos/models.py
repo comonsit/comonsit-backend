dfa38b677bc2bc753983dd08926540b474100b04
mauricioinaz <mauricio.glez@gmail.com>
2020-01-29 19:58:30 -0500

    DJ: update field in movimientos model

--- movimientos/models.py
+++ movimientos/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Movimiento(models.Model):
+    EFECTIVO = 'EF'
+    BANCOS = 'BA'
+    TRANSFERENCIA = 'TR'
+    MOVIMIENTO_CHOICES = [
+        (EFECTIVO, 'Efectivo'),
+        (BANCOS, 'Bancos'),
+        (TRANSFERENCIA, 'Transferencia')
+    ]
     id = models.AutoField(primary_key=True)
     clave_socio = models.ForeignKey('socios.Socio', on_delete=models.CASCADE, blank=False, related_name='movimiento')
     fecha_entrega = models.DateField()
@@ -9,7 +17,7 @@ class Movimiento(models.Model):
     empresa = models.ForeignKey('tsumbalil.Empresa', on_delete=models.CASCADE, default=1)  # 1 = Ts'umbal
     fecha_banco = models.DateField()
     aportacion = models.BooleanField(blank=False, default=True)  # false = retiro
-    efectivo = models.BooleanField(blank=False, default=False, null=True)  # false = banco
+    tipo_de_movimiento = models.CharField(max_length=2, choices=MOVIMIENTO_CHOICES, blank=True)
 
     def __str__(self):
         if self.aportacion:
