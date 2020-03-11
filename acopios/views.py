f554f52fed3f7796b69bc7faf87da79f401998f2
mauricioinaz <mauricio.glez@gmail.com>
2020-03-11 16:49:26 -0400

    RE: acopios.xls download option.

--- acopios/views.py
+++ acopios/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets, permissions
+from drf_renderer_xlsx.mixins import XLSXFileMixin
+from drf_renderer_xlsx.renderers import XLSXRenderer
 
 from .models import Acopio
 from .permissions import gerenciaOrRegion
 from .serializers import AcopioSerializer
+from users.permissions import gerenciaOrReadOnly
 
 
 class AcopioViewSet(viewsets.ModelViewSet):
@@ -14,3 +17,11 @@ class AcopioViewSet(viewsets.ModelViewSet):
         if self.request.user.is_gerencia():
             return Acopio.objects.all().order_by('-fecha')
         return Acopio.objects.filter(clave_socio__comunidad__region=self.request.user.clave_socio.comunidad.region).order_by('-fecha')
+
+
+class AcopioViewSetXLSX(XLSXFileMixin, viewsets.ReadOnlyModelViewSet):
+    queryset = Acopio.objects.all()
+    serializer_class = AcopioSerializer
+    renderer_classes = [XLSXRenderer]
+    permission_classes = [permissions.IsAuthenticated, gerenciaOrReadOnly]
+    filename = 'acopios.xlsx'
