6aac23ea43a9ea5838b6d391a12dd3eed48cd130
mauricioinaz <mauricio.glez@gmail.com>
2020-03-11 17:00:50 -0400

    DJ: Acopios xls download query option by product, and fix permissions.

--- acopios/views.py
+++ acopios/views.py
@@ -5,7 +5,7 @@ from drf_renderer_xlsx.renderers import XLSXRenderer
 from .models import Acopio
 from .permissions import gerenciaOrRegion
 from .serializers import AcopioSerializer
-from users.permissions import gerenciaOrReadOnly
+from users.permissions import gerenciaOnly
 
 
 class AcopioViewSet(viewsets.ModelViewSet):
@@ -20,8 +20,14 @@ class AcopioViewSet(viewsets.ModelViewSet):
 
 
 class AcopioViewSetXLSX(XLSXFileMixin, viewsets.ReadOnlyModelViewSet):
-    queryset = Acopio.objects.all()
     serializer_class = AcopioSerializer
     renderer_classes = [XLSXRenderer]
-    permission_classes = [permissions.IsAuthenticated, gerenciaOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, gerenciaOnly]
     filename = 'acopios.xlsx'
+
+    def get_queryset(self):
+        queryset = Acopio.objects.all().order_by('-fecha')
+        type = self.request.query_params.get('tipo_de_producto', None)
+        if type:
+            queryset = queryset.filter(tipo_de_producto=type)
+        return queryset
