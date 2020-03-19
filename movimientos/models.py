94026005fa882bea4e57a62fb43b6ba173bb80d8
mauricioinaz <mauricio.glez@gmail.com>
2020-03-18 21:24:41 -0400

    DJ: Update movimiento model (COMPLETE!)

--- movimientos/models.py
+++ movimientos/models.py
@@ -10,14 +10,28 @@ class Movimiento(models.Model):
         (BANCOS, 'Bancos'),
         (TRANSFERENCIA, 'Transferencia')
     ]
+    # TODO: duplicated with acopios, centralize in single place
+    CAFE = 'CF'
+    MIEL = 'MI'
+    JABON = 'JA'
+    SUELDOS = 'SL'
+    PROCESO_CHOICES = [
+        (CAFE, 'Cafe'),
+        (MIEL, 'Miel'),
+        (JABON, 'Jabon'),
+        (SUELDOS, 'Sueldos'),
+    ]
     id = models.AutoField(primary_key=True)
     clave_socio = models.ForeignKey('socios.Socio', on_delete=models.CASCADE, blank=False, related_name='movimiento')
     fecha_entrega = models.DateField()
     monto = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
-    empresa = models.ForeignKey('tsumbalil.Empresa', on_delete=models.CASCADE, default=1)  # 1 = Ts'umbal
-    fecha_banco = models.DateField()
+    proceso = models.CharField(max_length=2, choices=PROCESO_CHOICES, default='CF')
     aportacion = models.BooleanField(blank=False, default=True)  # false = retiro
     tipo_de_movimiento = models.CharField(max_length=2, choices=MOVIMIENTO_CHOICES, blank=True)
+    responsable_entrega = models.CharField(max_length=50, blank=True)
+    fecha_banco = models.DateField(blank=True, null=True)
+    referencia_banco = models.CharField(max_length=20, blank=True, null=True)
+    autor = models.ForeignKey('users.User', on_delete=models.CASCADE, blank=False, related_name='movimiento_autor')
 
     def __str__(self):
         if self.aportacion:
