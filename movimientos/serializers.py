6888b7b0d8921a717a7dff205fde797b546378e1
mauricioinaz <mauricio.glez@gmail.com>
2020-03-19 14:41:38 -0400

    DJ: inValidate Movimientos if Socio not active in selected process.

--- movimientos/serializers.py
+++ movimientos/serializers.py
@@ -8,3 +8,19 @@ class MovimientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movimiento
         fields = "__all__"
+
+    def validate(self, data):
+        """
+        Check that user is active in requested process
+        """
+        # TODO: Move to a general access
+        procesos = {
+            'CF': 'estatus_cafe',
+            'MI': 'estatus_miel',
+            'JA': 'estatus_yip',
+            'SL': 'estatus_trabajador'
+        }
+        process_status = getattr(data['clave_socio'], procesos[data['proceso']])
+        if process_status != 'AC':
+            raise serializers.ValidationError("El Socio no está activo en este proceso")
+        return data
