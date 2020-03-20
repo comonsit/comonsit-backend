3798771e7d9a0a75c478d31fc94546eca98295a5
mauricioinaz <mauricio.glez@gmail.com>
2020-03-20 18:27:39 -0400

    DJ: limit Solicitud permissions, and queries.

---
+++ creditos/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+
+class SolicitudPermissions(BasePermission):
+    """
+    CRU
+    """
+    def has_permission(self, request, view):
+        if request.method == "POST" or request.method == "GET" or request.method == "PATCH":
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        # Is gerencia or Owner
+        return request.user.is_gerencia() or obj.promotor == request.user
--- creditos/views.py
+++ creditos/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import SolicitudCredito
+from .permissions import SolicitudPermissions
 from .serializers import SolicitudCreditoSerializer, SolicitudListSerializer, SolicitudPartialUpdateSerializer
 
 
@@ -8,6 +9,7 @@ class SolicitudCreditoViewSet(viewsets.ModelViewSet):
     queryset = SolicitudCredito.objects.all().order_by('-fecha_solicitud')
     serializer_class = SolicitudCreditoSerializer
     lookup_field = 'folio_solicitud'
+    permission_classes = [permissions.IsAuthenticated, SolicitudPermissions]
 
     def get_serializer_class(self):
         if self.action == 'partial_update':
@@ -18,3 +20,9 @@ class SolicitudCreditoViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(promotor=self.request.user)
+
+    # Is Gerencia or Owner
+    def get_queryset(self):
+        if self.request.user.is_gerencia():
+            return SolicitudCredito.objects.all().order_by('-fecha_solicitud')
+        return SolicitudCredito.objects.filter(promotor=self.request.user).order_by('-fecha_solicitud')
