940d7ee8f982d18b4f032b142ddb875f5c8aa5aa
mauricioinaz <mauricio.glez@gmail.com>
2020-04-15 17:45:02 -0400

    DJ:Contratos: Send extra data for individual GET

--- contratos/serializers.py
+++ contratos/serializers.py
@@ -7,6 +7,13 @@ class ContratoCreditoSerializer(serializers.ModelSerializer):
     deuda_al_dia = serializers.SerializerMethodField(read_only=True)
     estatus_detail = serializers.SerializerMethodField(read_only=True)
     fecha_vencimiento = serializers.SerializerMethodField(read_only=True)
+    nombres = serializers.SerializerMethodField(read_only=True)
+    region = serializers.SerializerMethodField(read_only=True)
+    comunidad = serializers.SerializerMethodField(read_only=True)
+    proceso = serializers.SerializerMethodField(read_only=True)
+    tipo_credito = serializers.SerializerMethodField(read_only=True)
+    intereses = serializers.SerializerMethodField(read_only=True)
+    total = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ContratoCredito
@@ -20,6 +27,30 @@ class ContratoCreditoSerializer(serializers.ModelSerializer):
 
     def get_fecha_vencimiento(self, object):
         return object.fecha_vencimiento()
+
+    def get_nombres(self, object):
+        return object.clave_socio.nombres + ' ' + object.clave_socio.apellido_paterno \
+                + ' ' + object.clave_socio.apellido_materno
+
+    def get_comunidad(self, object):
+        return object.clave_socio.comunidad.nombre_de_comunidad
+
+    def get_region(self, object):
+        return object.clave_socio.comunidad.region.id
+
+    def get_proceso(self, object):
+        return object.solicitud.get_proceso_display()
+
+    def get_tipo_credito(self, object):
+        return object.solicitud.get_tipo_credito_display()
+
+    # TODO: Check if should use deuda_calculator instead!!
+    # with the next two
+    def get_intereses(self, object):
+        return object.monto*(object.tasa/100)*object.plazo
+
+    def get_total(self, object):
+        return object.monto + object.monto*(object.tasa/100)*object.plazo
 
     # def update(self, instance, validated_data):
     #     pass
