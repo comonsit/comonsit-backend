89c90db61f4cdc98bda4a608b3230c462e83db6c
mauricioinaz <mauricio.glez@gmail.com>
2020-04-19 19:16:58 -0400

    DJ:DeudaCalculator: Include variable and fixed interest.

--- contratos/models.py
+++ contratos/models.py
@@ -23,6 +23,13 @@ class ContratoCredito(models.Model):
         (CANCELADO, 'Cancelado')
     ]
 
+    FIJA = 'FI'
+    VARIABLE = 'VA'
+    TASA_CHOICES = [
+        (FIJA, 'Fija'),
+        (VARIABLE, 'Variable')
+    ]
+
     folio = models.AutoField(primary_key=True)
     solicitud = models.OneToOneField('solicitudes.SolicitudCredito', on_delete=models.CASCADE, blank=False, related_name='contrato')
     clave_socio = models.ForeignKey('socios.Socio', on_delete=models.CASCADE, blank=False, related_name='contrato')
@@ -31,6 +38,7 @@ class ContratoCredito(models.Model):
     plazo = models.PositiveSmallIntegerField(blank=False)  # number of months
     tasa = models.DecimalField(max_digits=7, decimal_places=4, blank=False)
     tasa_moratoria = models.DecimalField(max_digits=7, decimal_places=4, blank=False)
+    tipo_tasa = models.CharField(max_length=2, choices=TASA_CHOICES, blank=False)
     prorroga = models.PositiveSmallIntegerField(blank=False, default=0)  # number of months
     estatus = models.CharField(max_length=2, choices=ESTATUS_CHOICES, blank=False)
     referencia_banco = models.CharField(max_length=20, blank=True, null=True)
--- contratos/utility.py
+++ contratos/utility.py
@@ -1,7 +1,9 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from django.db.models import Sum
 from rest_framework.serializers import ValidationError
 from .models import ContratoCredito
+from pagos.models import Pago
 
 
 def deuda_calculator(credito, fecha=date.today()):
@@ -15,15 +17,22 @@ def deuda_calculator(credito, fecha=date.today()):
         raise ValidationError("La fecha es previa al inicio del Crédito")
 
     meses_transcurridos = relativedelta(fecha, credito.fecha_inicio).months
-    interes_ordinario = credito.monto*(credito.tasa/100)*meses_transcurridos
 
+    if credito.tipo_tasa == ContratoCredito.FIJA:
+        capital = credito.monto
+    elif credito.tipo_tasa == ContratoCredito.VARIABLE:
+        pagado = Pago.objects.filter(credito=credito).aggregate(Sum('cantidad'))['cantidad__sum']
+        capital = credito.monto - pagado
+
+    interes_ordinario = capital*(credito.tasa/100)*meses_transcurridos
     plazo_total = credito.plazo + credito.prorroga
     interes_moratorio = 0
+
     if fecha > credito.fecha_vencimiento():
-        interes_moratorio = credito.monto*(credito.tasa/100)*(meses_transcurridos-plazo_total)
+        interes_moratorio = capital*(credito.tasa_moratoria/100)*(meses_transcurridos-plazo_total)
 
     return {
-            'total': credito.monto + interes_ordinario + interes_moratorio,
+            'total': capital + interes_ordinario + interes_moratorio,
             'interes_ordinario': interes_ordinario,
             'interes_moratorio': interes_moratorio,
             }
