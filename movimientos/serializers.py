648c23ba78fe6e615528d75ad1331ec7af531d4d
mauricioinaz <mauricio.glez@gmail.com>
2020-05-04 14:23:07 -0400

    RE/DJ: Only Gerencia que make withdrawals in Movimientos

--- movimientos/serializers.py
+++ movimientos/serializers.py
@@ -23,4 +23,10 @@ class MovimientoSerializer(serializers.ModelSerializer):
         process_status = getattr(data['clave_socio'], procesos[data['proceso']])
         if process_status != 'AC':
             raise serializers.ValidationError("El Socio no está activo en este proceso")
+
+        aportacion = data.get('aportacion')
+        current_user = self.context['request'].user
+        if not aportacion and not current_user.is_gerencia():
+            raise serializers.ValidationError("Solo gerencia puede registrar Retiros")
+
         return data
