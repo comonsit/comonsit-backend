dabdcebf01424e04fa8b4289171e8a20542c61d6
mauricioinaz <mauricio.glez@gmail.com>
2020-05-13 14:48:17 -0400

    DJ:Pagos: update permissions.

--- pagos/permissions.py
+++ pagos/permissions.py
@@ -1,25 +1,13 @@
 from rest_framework.permissions import BasePermission
-from socios.models import Socio
 
 
 class gerenciaOrRegion(BasePermission):
     """
-    CR   permission for Gerencia
-    CR   pemission per Region
+    CRU   permission for Gerencia
+    CRU   pemission per Region
     """
     def has_permission(self, request, view):
-        if request.method == "GET":
-            return True
-        elif request.method == "POST":
-            if request.user.is_gerencia():
-                return True
-            # TODO: Consider moving to create in Serializer
-            elif request.data['clave_socio']:
-                return Socio.objects.get(clave_socio=request.data['clave_socio']).comunidad.region == request.user.clave_socio.comunidad.region
-            return False
-        elif request.method == "PUT" or request.method == "PATCH":
-            return request.user.is_gerencia()
-        return False
+        return request.method == "GET" or request.method == "POST" or request.method == "PATCH"
 
     def has_object_permission(self, request, view, obj):
         if request.user.is_gerencia():
