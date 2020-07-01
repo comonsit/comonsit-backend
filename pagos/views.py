4e8d82531d8fd378afbb62a8f8055b3395111449
mauricioinaz <mauricio.glez@gmail.com>
2020-07-01 17:12:49 -0400

    DJ:Pagos: Add filtered unlinked to bank list view

--- pagos/views.py
+++ pagos/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from drf_renderer_xlsx.mixins import XLSXFileMixin
 from drf_renderer_xlsx.renderers import XLSXRenderer
 
@@ -21,14 +23,22 @@ class PagoViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         if self.request.user.is_gerencia():
             queryset = Pago.objects.all().order_by('-fecha_pago')
+            if self.action == 'no_link':
+                queryset = queryset.filter(registrocontable__isnull=True)
         else:
-            queryset = Pago.objects.filter(credito__clave_socio__comunidad__region=self.request.user.clave_socio.comunidad.region).order_by('-fecha_pago')
+            queryset = queryset.filter(credito__clave_socio__comunidad__region=self.request.user.clave_socio.comunidad.region)
         # TODO: Give Response of unAuthorized socio Search.
         clave_socio = self.request.query_params.get('clave_socio', None)
         if clave_socio:
             queryset = queryset.filter(clave_socio=clave_socio)
         # TODO: limit view if no query to ???
         return queryset
+
+    @action(methods=['get'], detail=False, url_path='no-link', url_name='no-link')
+    def no_link(self, request):
+        q = self.get_queryset()
+        serializer = self.get_serializer(q, many=True)
+        return Response(serializer.data)
 
 
 class PagoViewSetXLSX(XLSXFileMixin, viewsets.ReadOnlyModelViewSet):
