41ceb0ca1558f7aacca4bcdad01ad3a3d80c1501
mauricioinaz <mauricio.glez@gmail.com>
2020-09-29 12:16:30 -0400

    DJ:Movimientos: Validatie balance is greater than retiring funds

--- movimientos/serializers.py
+++ movimientos/serializers.py
@@ -1,5 +1,8 @@
 from datetime import date
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
 from rest_framework import serializers
+
 from .models import Movimiento
 from comonSitDjango.constants import PROCESOS_FIELDS, ACTIVO
 
@@ -15,14 +18,27 @@ class MovimientoSerializer(serializers.ModelSerializer):
         """
         Check that user is active in requested process
         """
-        process_status = getattr(data['clave_socio'], PROCESOS_FIELDS[data['proceso']])
+        socio = data.get('clave_socio')
+        process_status = getattr(socio, PROCESOS_FIELDS[data['proceso']])
         if process_status != ACTIVO:
             raise serializers.ValidationError("El Socio no está activo en este proceso")
 
         aportacion = data.get('aportacion')
+        monto = data.get('monto')
         current_user = self.context['request'].user
-        if not aportacion and not current_user.is_gerencia():
-            raise serializers.ValidationError("Solo gerencia puede registrar Retiros")
+        # Validation for RETIROS
+        if not aportacion:
+            # only Gerencia can save RETIROS
+            if not current_user.is_gerencia():
+                raise serializers.ValidationError("Solo gerencia puede registrar Retiros")
+
+            # RETIRO Cannot by greater than available balance
+            q = Movimiento.objects.filter(clave_socio=socio)
+            aports = q.filter(aportacion=True).aggregate(total=Coalesce(Sum('monto'), 0))['total']
+            retiros = q.filter(aportacion=False).aggregate(total=Coalesce(Sum('monto'), 0))['total']
+            balance = aports - retiros
+            if monto > balance:
+                raise serializers.ValidationError({'monto': f'No se puede hacer un retiro mayor al saldo actual: ${balance}'})
 
         fecha_entrega = data.get('fecha_entrega')
         tipo_de_movimiento = data.get('tipo_de_movimiento')
@@ -30,7 +46,7 @@ class MovimientoSerializer(serializers.ModelSerializer):
         if (tipo_de_movimiento == Movimiento.EFECTIVO and
                 (fecha_entrega.year != today.year or
                  fecha_entrega.month != today.month)):
-            raise serializers.ValidationError("Las aportaciones en efectivo sólo pueden ser del mes en curso.")
+            raise serializers.ValidationError({"fecha_entrega": "Las aportaciones en efectivo sólo pueden ser del mes en curso."})
 
         return data
 
